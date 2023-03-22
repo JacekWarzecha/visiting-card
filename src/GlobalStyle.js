745aafb1dc59ef9fe5a6ad970d271ba5ed45140d
JacekWarzecha <drjackys@gmail.com>
2023-03-22 13:36:14 +0100

    GlobalStyle and theme added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+  
+*,
+::after,
+::before {
+ box-sizing: inherit;
+}
+
+#root {
+    min-height: 100vh;
+    background: gold;
+    
+}
+`;
